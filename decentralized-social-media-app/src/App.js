540e4f7f1d48c2217899d4bd6d123fed038e2a27
Gaurav Singh <76627244+Thatsgaurav@users.noreply.github.com>
2022-12-21 17:37:33 +0530

    commit

--- decentralized-social-media-app/src/App.js
+++ decentralized-social-media-app/src/App.js
@@ -12,9 +12,7 @@ import { Box, Button, Image } from "@chakra-ui/react";
 function App() {
   const [account, setAccount] = useState(null);
   const [profiles, setProfiles] = useState([]);
-  console.log("~ profiles", profiles);
   const [posts, setPosts] = useState([]);
-  console.log("~ posts", posts);
 
   async function signIn() {
     const accounts = await window.ethereum.request({
@@ -42,6 +40,7 @@ function App() {
     });
     setPosts(posts);
   }
+
   async function follow(id) {
     const provider = new ethers.providers.Web3Provider(window.ethereum);
     const contract = new ethers.Contract(
@@ -58,8 +57,23 @@ function App() {
     getPosts();
   }, []);
 
+  const parseImageUrl = (profile) => {
+    if (profile) {
+      const url = profile.picture?.original?.url;
+      if (url && url.startsWith("ipfs:")) {
+        const ipfsHash = url.split("//")[1];
+        return `https://gateway.pinata.cloud/ipfs/${ipfsHash}`;
+      }
+
+      return url;
+    }
+
+    return "/default-avatar.png";
+  };
+
   return (
     <div className="app">
+      {/* NAVBAR */}
       <Box width="100%" backgroundColor="rgba(5, 32, 64, 28)">
         <Box
           display="flex"
@@ -76,12 +90,12 @@ function App() {
               fontSize="44px"
               fontStyle="italic"
             >
-              DECENTRA
+              TALKDAAPS
             </Box>
-            <Box> DECENTRALIZED SOCIAL MEDIA APP</Box>
+            <Box> Decentralized Social Media App</Box>
           </Box>
           {account ? (
-            <Box backgroundColor="000" padding="15px" borderRadius="6px">
+            <Box backgroundColor="#000" padding="15px" borderRadius="6px">
               Connected
             </Box>
           ) : (
@@ -95,10 +109,105 @@ function App() {
           )}
         </Box>
       </Box>
+
+      {/* CONTENT */}
+      <Box
+        display="flex"
+        justifyContent="space-between"
+        width="55%"
+        margin="35px auto auto auto"
+        color="white"
+      >
+        {/* POSTS */}
+        <Box width="65%" maxWidth="65%" minWidth="65%">
+          {posts.map((post) => (
+            <Box
+              key={post.id}
+              marginBottom="25px"
+              backgroundColor="rgba(5, 32, 64, 28)"
+              padding="40px 30px 40px 25px"
+              borderRadius="6px"
+            >
+              <Box display="flex">
+                {/* PROFILE IMAGE */}
+                <Box width="75px" height="75px" marginTop="8px">
+                  <img
+                    alt="profile"
+                    src={parseImageUrl(post.profile)}
+                    width="75px"
+                    height="75px"
+                    onError={({ currentTarget }) => {
+                      currentTarget.onerror = null; // prevents looping
+                      currentTarget.src = "/default-avatar.png";
+                    }}
+                  />
+                </Box>
+
+                {/* POST CONTENT */}
+                <Box flexGrow={1} marginLeft="20px">
+                  <Box display="flex" justifyContent="space-between">
+                    <Box fontFamily="DM Serif Display" fontSize="24px">
+                      {post.profile?.handle}
+                    </Box>
+                    <Box height="50px" _hover={{ cursor: "pointer" }}>
+                      <Image
+                        alt="follow-icon"
+                        src="/follow-icon.png"
+                        width="50px"
+                        height="50px"
+                        onClick={() => follow(post.id)}
+                      />
+                    </Box>
+                  </Box>
+                  <Box overflowWrap="anywhere" fontSize="14px">
+                    {post.metadata?.content}
+                  </Box>
+                </Box>
+              </Box>
+            </Box>
+          ))}
+        </Box>
+
+        {/* FRIEND SUGGESTIONS */}
+        <Box
+          width="30%"
+          backgroundColor="rgba(5, 32, 64, 28)"
+          padding="40px 25px"
+          borderRadius="6px"
+          height="fit-content"
+        >
+          <Box fontFamily="DM Serif Display">FRIEND SUGGESTIONS</Box>
+          <Box>
+            {profiles.map((profile, i) => (
+              <Box
+                key={profile.id}
+                margin="30px 0"
+                display="flex"
+                alignItems="center"
+                height="40px"
+                _hover={{ color: "#808080", cursor: "pointer" }}
+              >
+                <img
+                  alt="profile"
+                  src={parseImageUrl(profile)}
+                  width="40px"
+                  height="40px"
+                  onError={({ currentTarget }) => {
+                    currentTarget.onerror = null; // prevents looping
+                    currentTarget.src = "/default-avatar.png";
+                  }}
+                />
+                <Box marginLeft="25px">
+                  <h4>{profile.name}</h4>
+                  <p>{profile.handle}</p>
+                </Box>
+              </Box>
+            ))}
+          </Box>
+        </Box>
+      </Box>
     </div>
   );
 }
 
 export default App;
-
-// https://youtu.be/FiZIGm41dgE?t=1414
